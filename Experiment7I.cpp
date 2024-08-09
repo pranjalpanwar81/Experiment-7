153f653aad9a6488054c1113d5c1af65822d9f2b
Pranjal Panwar <138666385+pranjalpanwar81@users.noreply.github.com>
2024-08-09 11:22:42 +0530

    Create Experiment7I.cpp

---
+++ Experiment7I.cpp
@@ -0,0 +1,20 @@
+// NAME - Pranjal Panwar
+// PRM - 23070123164
+// EXPERIMENT - 7(I) 
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+string a;
+cout<<"Enter string: ";
+cin>>a;
+int i;
+for(i=a.length()-1;i>=0;i--) {
+cout<<a[i];
+}
+return 0;
+}
+
+// Enter string: Shloka 
+// akolhS 
