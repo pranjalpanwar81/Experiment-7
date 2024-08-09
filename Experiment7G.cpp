3b176f7553a09026047cc1c652c4eb1d7555dfdb
Pranjal Panwar <138666385+pranjalpanwar81@users.noreply.github.com>
2024-08-09 11:20:53 +0530

    Create Experiment7G.cpp

---
+++ Experiment7G.cpp
@@ -0,0 +1,19 @@
+// NAME - Pranjal Panwar
+// PRM - 23070123164
+// EXPERIMENT - 7(G) 
+
+#include<iostream> 
+#include<string>
+using namespace std; 
+
+int main() 
+{
+    string a;
+    cout<<"Enter any word: "; 
+    cin>>a;
+    cout<<"Entered string is "<<a<<"."<<"\n";
+    return 0;
+}
+
+// Enter any word: Environment
+// Entered string is Environment.
